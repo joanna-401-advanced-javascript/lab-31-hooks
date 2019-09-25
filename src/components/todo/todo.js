a2b3ceaf06f0d91676de9a26620080a8cd3e9583
joannaarroyo <joliuac@gmail.com>
2019-09-24 22:28:23 -0700

    refactored todo, using useState and useReducer

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,63 +1,69 @@
-import React from 'react';
+import React, { useState, useReducer} from 'react';
 
 import Auth from '../auth/auth.js';
 
 import styles from './todo.module.scss';
 
-class Todo extends React.Component {
+const initialState = {
+  item: '',
+  toDoItems: [],
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      item: '',
-      toDoItems: []
-    }
-  }
-
-  handleForm = (e) => {
-    e.preventDefault();
-    e.target.reset();
-    let item = { title:this.state.item, status:false };
-    this.setState({toDoItems:[...this.state.toDoItems, item]});
-  };
-
-  handleChange = (e) => {
-    this.setState( {item:e.target.value} );
-  };
-
-  toggle = (e,id) => {
-    e.preventDefault();
-    let toDoItems = this.state.toDoItems.map( (item,idx) =>
-      idx === id ? {title:item.title, status:!item.status} : item
-    );
-    this.setState({toDoItems});
-  };
-
-  render() {
-    return (
-      <section className={styles.todo}>
-
-        <Auth capability="read">
-          {this.state.toDoItems.map((item, idx) =>
-            <div key={idx} onClick={(e) => this.toggle(e, idx)}>
-              <span className={styles[`complete-${item.status}`]}> {item.title} </span>
-            </div>
-          )}
-        </Auth>
-
-        <Auth capability="create">
-          <form onSubmit={this.handleForm}>
-            <input
-              onChange={this.handleChange}
-              name="item"
-              placeholder="Add To Do List Item Here"
-            />
-          </form>
-        </Auth>
-
-      </section>
-    );
+function reducer(state, action) {
+  switch(action.type){
+    case 'toggle':
+      return {toDoItems: action.data};
+    case 'form':
+      return {...state, toDoItems: [...state.toDoItems, action.data]};
+    default:
+      throw new Error();
   }
 }
 
-export default Todo;
+export default function Todo(props) {
+  const [item, setItem] = useState(initialState.item);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function handleForm(e){
+    e.preventDefault();
+    e.target.reset();
+    let itemObj = { title: item, status: false };
+    dispatch({type: 'form', data: itemObj});
+  }
+
+  function handleChange(e){
+    setItem(e.target.value);
+  }
+
+  function toggle(e, id){
+    e.preventDefault();
+    let toDoItems = state.toDoItems.map( (item, idx) =>
+      idx === id ? {title: item.title, status: !item.status} : item
+    );
+    dispatch({type: 'toggle', data: toDoItems})
+  }
+
+  return (
+    <section className={styles.todo}>
+
+      <Auth capability="read">
+        {state.toDoItems.map((item, idx) =>
+          <div key={idx} onClick={(e) => toggle(e, idx)}>
+            <span className={styles[`complete-${item.status}`]}> {item.title} </span>
+          </div>
+        )}
+      </Auth>
+
+      <Auth capability="create">
+        <form onSubmit={handleForm}>
+          <input
+            onChange={handleChange}
+            name="item"
+            placeholder="Add To Do List Item Here"
+          />
+        </form>
+      </Auth>
+
+    </section>
+  );
+}
